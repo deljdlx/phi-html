574ae7a5df89284975c42368e40a526ff99791b1
Jean luc <you@example.com>
2019-02-21 09:56:52 +0100

    synchro

---
+++ source/class/Element/Ol.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Phi\HTML\Element;
+
+
+use Phi\HTML\Element;
+
+class Ol extends Element
+{
+
+    public function __construct()
+    {
+        parent::__construct('ol');
+    }
+
+
+
+
+
+}
---
+++ source/class/Element/Ul.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Phi\HTML\Element;
+
+
+use Phi\HTML\Element;
+
+class Ul extends Element
+{
+
+    public function __construct()
+    {
+        parent::__construct('ul');
+    }
+
+
+
+
+
+}
